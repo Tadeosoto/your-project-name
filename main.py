f6e797b2b450653ea197d9309ff237399dd4c5af
Tadeosoto <tadeosoto1993@gmail.com>
2026-04-01 00:27:55 -0600

    2nd commit to asteroid game

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
 import pygame
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
-from constants import (SCREEN_HEIGHT, SCREEN_WIDTH)
+
 
 def main():
-    pygame.init() 
+    pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         log_state()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-    screen.fill('black')
-    print(f"Starting Asteroids ")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-    screen.display.flip()
+
+        screen.fill("black")
+        pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
