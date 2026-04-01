38e725c781a5e36dec733b5a9f629b51d39f17e6
Tadeosoto <tadeosoto1993@gmail.com>
2026-04-01 02:22:18 -0600

    movement added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+from player import Player
 
 
 def main():
@@ -8,7 +9,10 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
-
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2 )
     while True:
         log_state()
 
@@ -17,6 +21,10 @@ def main():
                 return
 
         screen.fill("black")
+        updatable.update(dt)
+        # player.draw(screen)
+        for draw in drawable:
+            draw.draw(screen)
         pygame.display.flip()
 
         # limit the framerate to 60 FPS
